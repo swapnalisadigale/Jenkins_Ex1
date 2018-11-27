48f90473fa6ff4d2b7e3fa96cadc1970d6ab937b
Swapnali Sadigale <swapnali.sadigale@EIC.EINFOCHIPS.COM>
2018-11-27 16:40:26 +0530

    First version of EX1.c

---
+++ EX1.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int sum;
+    sum = 6 + 7;
+    printf("Sum is %d\n", sum);
+    return 0;
+}
